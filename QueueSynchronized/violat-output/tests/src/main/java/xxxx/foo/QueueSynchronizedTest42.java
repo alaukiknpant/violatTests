f6051076b5414865aa91bad062e961fb8cf5c261
User <user@Kim-Dong-Jung-iMac.local>
2021-02-03 01:22:27 +0800

    Added new issues

---
+++ QueueSynchronized/violat-output/tests/src/main/java/xxxx/foo/QueueSynchronizedTest42.java
@@ -0,0 +1,42 @@
+package xxxx.foo;
+
+import java.util.Arrays;
+import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import com.foo.QueueSynchronized;
+import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.*;
+
+@JCStressTest
+@Outcome(
+    id = "",
+    expect = Expect.ACCEPTABLE,
+    desc = "atomic")
+@State
+public class QueueSynchronizedTest42 {
+    QueueSynchronized obj = new QueueSynchronized();
+    
+    @Actor
+    public void actor0(StringResult0 result) {
+        String r;
+        
+        try { obj.put(0); }
+        catch (Exception e) { }
+        
+        try { obj.put(1); }
+        catch (Exception e) { }
+        
+        try { obj.put(1); }
+        catch (Exception e) { }
+    }
+    
+    @Actor
+    public void actor1(StringResult0 result) {
+        String r;
+        
+        try { obj.put(0); }
+        catch (Exception e) { }
+    }
+}
---
+++ QueueSynchronized/violat-output/tests/target/generated-sources/annotations/xxxx/foo/QueueSynchronizedTest25_jcstress.java
@@ -0,0 +1,189 @@
+package xxxx.foo;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jcstress.infra.runners.TestConfig;
+import org.openjdk.jcstress.infra.collectors.TestResultCollector;
+import org.openjdk.jcstress.infra.runners.Runner;
+import org.openjdk.jcstress.infra.runners.StateHolder;
+import org.openjdk.jcstress.util.Counter;
+import org.openjdk.jcstress.vm.WhiteBoxSupport;
+import org.openjdk.jcstress.util.OpenAddressHashCounter;
+import java.util.concurrent.ExecutionException;
+import xxxx.foo.QueueSynchronizedTest25;
+import org.openjdk.jcstress.infra.results.StringResult1_jcstress;
+
+public class QueueSynchronizedTest25_jcstress extends Runner<StringResult1_jcstress> {
+
+    OpenAddressHashCounter<StringResult1_jcstress> counter_actor0;
+    OpenAddressHashCounter<StringResult1_jcstress> counter_actor1;
+    volatile StateHolder<Pair> version;
+
+    public QueueSynchronizedTest25_jcstress(TestConfig config, TestResultCollector collector, ExecutorService pool) {
+        super(config, collector, pool, "xxxx.foo.QueueSynchronizedTest25");
+    }
+
+    @Override
+    public void sanityCheck() throws Throwable {
+        sanityCheck_API();
+        sanityCheck_Footprints();
+    }
+
+    private void sanityCheck_API() throws Throwable {
+        final QueueSynchronizedTest25 t = new QueueSynchronizedTest25();
+        final QueueSynchronizedTest25 s = new QueueSynchronizedTest25();
+        final StringResult1_jcstress r = new StringResult1_jcstress();
+        Collection<Future<?>> res = new ArrayList<>();
+        res.add(pool.submit(() -> t.actor0(r)));
+        res.add(pool.submit(() -> t.actor1(r)));
+        for (Future<?> f : res) {
+            try {
+                f.get();
+            } catch (ExecutionException e) {
+                throw e.getCause();
+            }
+        }
+    }
+
+    private void sanityCheck_Footprints() throws Throwable {
+        config.adjustStrides(size -> {
+            version = new StateHolder<>(new Pair[size], 2, config.spinLoopStyle);
+            final QueueSynchronizedTest25 t = new QueueSynchronizedTest25();
+            for (int c = 0; c < size; c++) {
+                Pair p = new Pair();
+                p.r = new StringResult1_jcstress();
+                p.s = new QueueSynchronizedTest25();
+                version.pairs[c] = p;
+                p.s.actor0(p.r);
+                p.s.actor1(p.r);
+            }
+        });
+    }
+
+    @Override
+    public Counter<StringResult1_jcstress> internalRun() {
+        version = new StateHolder<>(new Pair[0], 2, config.spinLoopStyle);
+        counter_actor0 = new OpenAddressHashCounter<>();
+        counter_actor1 = new OpenAddressHashCounter<>();
+
+        control.isStopped = false;
+        Collection<Future<?>> tasks = new ArrayList<>();
+        tasks.add(pool.submit(this::actor0));
+        tasks.add(pool.submit(this::actor1));
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(config.time);
+        } catch (InterruptedException e) {
+        }
+
+        control.isStopped = true;
+
+        waitFor(tasks);
+
+        Counter<StringResult1_jcstress> counter = new OpenAddressHashCounter<>();
+        counter.merge(counter_actor0);
+        counter.merge(counter_actor1);
+        return counter;
+    }
+
+    public final void jcstress_consume(StateHolder<Pair> holder, OpenAddressHashCounter<StringResult1_jcstress> cnt, int a, int actors) {
+        Pair[] pairs = holder.pairs;
+        int len = pairs.length;
+        int left = a * len / actors;
+        int right = (a + 1) * len / actors;
+        for (int c = left; c < right; c++) {
+            Pair p = pairs[c];
+            StringResult1_jcstress r = p.r;
+            QueueSynchronizedTest25 s = p.s;
+            p.s = new QueueSynchronizedTest25();
+            cnt.record(r);
+            r.r1 = null;
+        }
+    }
+
+    public final void jcstress_updateHolder(StateHolder<Pair> holder) {
+        if (!holder.tryStartUpdate()) return;
+        Pair[] pairs = holder.pairs;
+        int len = pairs.length;
+
+        int newLen = holder.updateStride ? Math.max(config.minStride, Math.min(len * 2, config.maxStride)) : len;
+
+        Pair[] newPairs = pairs;
+        if (newLen > len) {
+            newPairs = Arrays.copyOf(pairs, newLen);
+            for (int c = len; c < newLen; c++) {
+                Pair p = new Pair();
+                p.r = new StringResult1_jcstress();
+                p.s = new QueueSynchronizedTest25();
+                newPairs[c] = p;
+            }
+         }
+
+        version = new StateHolder<>(control.isStopped, newPairs, 2, config.spinLoopStyle);
+        holder.finishUpdate();
+   }
+
+    public final Void actor0() {
+
+        OpenAddressHashCounter<StringResult1_jcstress> counter = counter_actor0;
+        while (true) {
+            StateHolder<Pair> holder = version;
+            if (holder.stopped) {
+                return null;
+            }
+
+            Pair[] pairs = holder.pairs;
+
+            holder.preRun();
+
+            for (Pair p : pairs) {
+                StringResult1_jcstress r = p.r;
+                r.trap = 0;
+                p.s.actor0(r);
+            }
+
+            holder.postRun();
+
+            jcstress_consume(holder, counter, 0, 2);
+            jcstress_updateHolder(holder);
+
+            holder.postUpdate();
+        }
+    }
+
+    public final Void actor1() {
+
+        OpenAddressHashCounter<StringResult1_jcstress> counter = counter_actor1;
+        while (true) {
+            StateHolder<Pair> holder = version;
+            if (holder.stopped) {
+                return null;
+            }
+
+            Pair[] pairs = holder.pairs;
+
+            holder.preRun();
+
+            for (Pair p : pairs) {
+                StringResult1_jcstress r = p.r;
+                r.trap = 0;
+                p.s.actor1(r);
+            }
+
+            holder.postRun();
+
+            jcstress_consume(holder, counter, 1, 2);
+            jcstress_updateHolder(holder);
+
+            holder.postUpdate();
+        }
+    }
+
+    static class Pair {
+        public QueueSynchronizedTest25 s;
+        public StringResult1_jcstress r;
+    }
+}
